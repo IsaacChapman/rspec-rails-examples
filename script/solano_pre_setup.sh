df427a2c084fe70585f8786c3dbd75e9f41a3a58
Isaac Chapman <isaac@isaacchapman.com>
2017-10-11 14:30:33 -0700

    Add storing of cache keys for usage in subsequent builds

--- script/solano_pre_setup.sh
+++ script/solano_pre_setup.sh
@@ -8,21 +8,19 @@ DB_DUMP_FILE=$TDDIUM_REPO_ROOT/tmp/db_dump.sql
 
 set -o errexit -o pipefail # Exit on errors
 
-alias date="date -u '+%Y-%m-%d %H:%M:%S UTC'" # For logging purposes
-
 hash_keys_match() {
   key=$1
   if [ ! -f $KEY_DIR_SESSION/$key ]; then
-  	echo "NOTICE: No session cache key file for '${key}'"
-  	return 1
+    echo "NOTICE: No session cache key file for '${key}'"
+    return 1
   fi
   if [ ! -f $KEY_DIR_STORED/$key ]; then
-  	echo "NOTICE: No stored cache key file for '${key}'"
-  	return 2
+    echo "NOTICE: No stored cache key file for '${key}'"
+    return 2
   fi
   if [[ "`cat $KEY_DIR_SESSION/$key`" != "`cat $KEY_DIR_STORED/$key`" ]]; then
-  	echo "NOTICE: Session and stored cache keys for '${key}' do not match"
-  	return 3
+    echo "NOTICE: Session and stored cache keys for '${key}' do not match"
+    return 3
   fi
 }
 
@@ -31,7 +29,7 @@ run_setup_task() {
   echo "NOTICE: `date -u '+%Y-%m-%d %H:%M:%S UTC'` - '$key' setup task started"
   time_start=`date +%s`
   case "$key" in
-  	bundle)
+    bundle)
       bundle install --path=$HOME/bundle --no-deployment
       bundle clean # Remove old/extraneous gems
       ;;
@@ -53,16 +51,31 @@ run_setup_task() {
    echo "NOTICE: `date -u '+%Y-%m-%d %H:%M:%S UTC'` - '$key' setup task completed in $((time_end - time_start)) second[s]"
 }
 
+store_cache_key() {
+  key=$1
+  mkdir -p $KEY_DIR_STORED
+  if [ -f $KEY_DIR_SESSION/$key ]; then
+    cp -f $KEY_DIR_SESSION/$key $KEY_DIR_STORED/$key
+  else
+    echo "NOTICE: '$key' does not exist at $KEY_DIR_SESSION/$key"
+  fi
+}
+
+
 for key in bundle node db; do
   if ! hash_keys_match $key; then
-  	run_setup_task $key
+    run_setup_task $key
   elif [[ "$key" == "db" ]]; then
-  	# Ensure the database is loaded
-  	if [ -f $DB_DUMP_FILE ]; then
-  	  mysql -u$TDDIUM_DB_USER -p$TDDIUM_DB_PASSWORD $TDDIUM_DB_NAME < $DB_DUMP_FILE
-  	else
-  	  echo "NOTICE: 'db' cache keys matched, but db_dump.sql file not present. Re-loading database"
-  	  run_setup_task db
-  	fi
+    # Ensure the database is loaded
+    if [ -f $DB_DUMP_FILE ]; then
+      echo "NOTICE: '$key' cache keys matched. Loading database from dump file."
+      mysql -u$TDDIUM_DB_USER -p$TDDIUM_DB_PASSWORD $TDDIUM_DB_NAME < $DB_DUMP_FILE
+    else
+      echo "NOTICE: '$key' cache keys matched, but db_dump.sql file not present. Re-loading database"
+      run_setup_task db
+    fi
+  else
+    echo "NOTICE: '$key' cache keys matched. Nothing to do."
   fi
+  store_cache_key $key
 done
